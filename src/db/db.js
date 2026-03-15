36d8b856ce3c67f4ce05c50309de34f1fe7597a6
potdaraayush <potdaraayush2023@gmail.com>
2026-03-15 13:53:19 +0530

    wrote db connection logic

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+const {Pool} = pg;
+
+const pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "url_shortener"
+});
+
+export default pool;
